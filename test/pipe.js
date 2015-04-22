169352f0a522ad5841bbcbb92728fbf14f3c266d
coderaiser <mnemonic.enemy@gmail.com>
2015-04-22 06:54:03 -0400

    test(pipe) add file1 | gzip | file2

--- test/pipe.js
+++ test/pipe.js
@@ -4,6 +4,7 @@
     var fs      = require('fs'),
         path    = require('path'),
         os      = require('os'),
+        zlib    = require('zlib'),
         
         pipe    = require('..'),
         test    = require('tape');
@@ -44,6 +45,28 @@
         tryPipe(__filename + random, nameTmp, function(error) {
             t.ok(error, error && error.message);
             
+            t.end();
+        });
+    });
+    
+    test('file1 | gzip | file2: no errors', function(t) {
+        var tmp         = os.tmpdir(),
+            random      = Math.random(),
+            name        = path.basename(__filename),
+            nameTmp     = path.join(tmp, name + random),
+            
+            read        = fs.createReadStream(__filename),
+            write       = fs.createWriteStream(nameTmp),
+            zip         = zlib.createGzip();
+        
+        pipe([read, zip, write], function() {
+            var file1   = fs.readFileSync(__filename, 'utf8'),
+                file2   = fs.readFileSync(nameTmp),
+                zip     = zlib.gzipSync(file1);
+            
+            fs.unlinkSync(nameTmp);
+            
+            t.deepEqual(zip, file2, 'file gziped');
             t.end();
         });
     });
